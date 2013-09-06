05a02111f9092b81e6abbb6c1fa5ea6ac9903426
john unwin <jdunwin@gmail.com>
2013-09-05 20:17:03 -0500

    Added Manager Test

---
+++ K2Drivers/K2Managers/ManagerTest/DriverManagerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ManagerTest
+{
+    [TestClass]
+    public class DriverManagerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
